0fa6d2e2befcc06bedda65e3b6ce9e48d8450d50
crespiiiy <moazmazen21@gmail.com>
2025-04-19 08:09:34 +0200

    Update config.js

--- public/config.js
+++ public/config.js
@@ -18,16 +18,16 @@ const config = {
     },
     dummyAccounts: {
         "Muse wallet - USD": "MUSE456789123",
-        "Orange Cash - EGP": "01061781445",
+        "Orange Cash - EGP": "https://t.me/Crespii_bot",
         "Payeer - USD": "P1130544382",
         "Payeer - RUB": "P1130544382",
-        "Vodafone Cash - EGP": "01061781445",
+        "Vodafone Cash - EGP": "https://t.me/Crespii_bot",
         "Binance pay - USDT": "BINANCE789456123",
         "BYBIT - USDT": "0x1B1468F18Ed86cB680e5A47D8e706D1464D243ED",
         "Cash Wallet - EGP": "0x88Ea63730a9b4C6228901DeEDC7e9834dd544490",
-        "Etisalat Cash - EGP": "01061781445",
+        "Etisalat Cash - EGP": "https://t.me/Crespii_bot",
         "Telegram - USD": "TVW2JZiszzHr4EyrbRAtCbU4cN9d13UvJN",
-        "Telegram - EGP": "TVW2JZiszzHr4EyrbRAtCbU4cN9d13UvJN",
+        "Telegram - EGP": "https://t.me/Crespii_bot",
         "Telegram - USDT": "TVW2JZiszzHr4EyrbRAtCbU4cN9d13UvJN",
         "Telegram - RUB": "TVW2JZiszzHr4EyrbRAtCbU4cN9d13UvJN"
     }
