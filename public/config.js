a251eec644802e2e08617b35e5d1bf19060f953f
crespiiiy <moazmazen21@gmail.com>
2025-04-19 07:02:24 +0200

    Add files via upload

---
+++ public/config.js
@@ -0,0 +1,34 @@
+const config = {
+    TELEGRAM_BOT_TOKEN: 'YOUR_TELEGRAM_BOT_TOKEN', // استبدل بمفتاح البوت من BotFather
+    CHAT_ID: 'YOUR_CHAT_ID', // استبدل بمعرف الدردشة
+    methodLimits: {
+        "Muse wallet - USD": { min: 10, max: 5000 },
+        "Orange Cash - EGP": { min: 100, max: 50000 },
+        "Payeer - USD": { min: 5, max: 10000 },
+        "Payeer - RUB": { min: 500, max: 500000 },
+        "Vodafone Cash - EGP": { min: 100, max: 50000 },
+        "Binance pay - USDT": { min: 10, max: 10000 },
+        "BYBIT - USDT": { min: 10, max: 10000 },
+        "Cash Wallet - EGP": { min: 100, max: 50000 },
+        "Etisalat Cash - EGP": { min: 100, max: 50000 },
+        "Telegram - USD": { min: 10, max: 5000 },
+        "Telegram - EGP": { min: 100, max: 50000 },
+        "Telegram - USDT": { min: 10, max: 10000 },
+        "Telegram - RUB": { min: 500, max: 500000 }
+    },
+    dummyAccounts: {
+        "Muse wallet - USD": "MUSE456789123",
+        "Orange Cash - EGP": "01061781445",
+        "Payeer - USD": "P1130544382",
+        "Payeer - RUB": "P1130544382",
+        "Vodafone Cash - EGP": "01061781445",
+        "Binance pay - USDT": "BINANCE789456123",
+        "BYBIT - USDT": "0x1B1468F18Ed86cB680e5A47D8e706D1464D243ED",
+        "Cash Wallet - EGP": "0x88Ea63730a9b4C6228901DeEDC7e9834dd544490",
+        "Etisalat Cash - EGP": "01061781445",
+        "Telegram - USD": "TVW2JZiszzHr4EyrbRAtCbU4cN9d13UvJN",
+        "Telegram - EGP": "TVW2JZiszzHr4EyrbRAtCbU4cN9d13UvJN",
+        "Telegram - USDT": "TVW2JZiszzHr4EyrbRAtCbU4cN9d13UvJN",
+        "Telegram - RUB": "TVW2JZiszzHr4EyrbRAtCbU4cN9d13UvJN"
+    }
+};
